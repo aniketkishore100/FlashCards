3f7ea3af0ec3f77200e6f5a0ee81e7a8c6565f70
aniketkishore100 <aniketkishore100@gmail.com>
2020-04-04 23:16:45 +0530

    added button to switch bw question and answer

--- routes/cards.js
+++ routes/cards.js
@@ -5,10 +5,22 @@ const {cards} = data;
  
  
 routes.get('/cards/:id',(req,res)=>{
-    res.render("card",{ 
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint
-    });
+    const side = req.query.side;
+    const id = req.params.id;
+    const text = cards[id][side]
+    const hint = cards[id].hint;
+
+    const templateData = {id,text}
+    if (side == 'question'){
+        templateData.hint  = hint;
+        templateData.sideToShow = 'answer';
+        templateData.sideToShowDisplay = 'Answer'
+    }
+    else if(side == 'answer'){
+        templateData.sideToShow = 'question';
+        templateData.sideToShowDisplay = 'Question'
+    }
+    res.render("card", templateData);
 })
 
 module.exports = routes; 
