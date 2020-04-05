f8db5c2d1aba2e5bb5a544c4baafec9e4e04099d
aniketkishore100 <aniketkishore100@gmail.com>
2020-04-05 13:47:09 +0530

    randomization of cards,added nextcard&begin buton

--- routes/cards.js
+++ routes/cards.js
@@ -3,24 +3,36 @@ const routes = express.Router()
 const { data }= require('../data/flashcardData.json')
 const {cards} = data;
  
- 
+routes.get('/cards',(req,res)=>{
+    const numberOfCards = cards.length;
+    const randmCard = Math.floor(Math.random()*numberOfCards);
+    res.redirect(`/cards/${randmCard}?side=question`)
+}) 
+
+
+
 routes.get('/cards/:id',(req,res)=>{
     const side = req.query.side;
     const id = req.params.id;
-    const text = cards[id][side]
-    const hint = cards[id].hint;
+    if(!side){
+        return res.redirect(`/cards/${id}?side=question`)
+    }
+        const text = cards[id][side]
+        const hint = cards[id].hint;
+        const name = req.cookies.username;
 
-    const templateData = {id,text}
-    if (side == 'question'){
-        templateData.hint  = hint;
-        templateData.sideToShow = 'answer';
-        templateData.sideToShowDisplay = 'Answer'
-    }
-    else if(side == 'answer'){
-        templateData.sideToShow = 'question';
-        templateData.sideToShowDisplay = 'Question'
-    }
-    res.render("card", templateData);
+        const templateData = {id,text,name}
+        if (side == 'question'){
+            templateData.hint  = hint;
+            templateData.sideToShow = 'answer';
+            templateData.sideToShowDisplay = 'Answer'
+        }
+        else if(side == 'answer'){
+            templateData.sideToShow = 'question';
+            templateData.sideToShowDisplay = 'Question'
+        }
+        res.render("card", templateData);
+    
 })
 
 module.exports = routes; 
